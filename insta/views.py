976c30087adfafd3ec9c3201547c44e5debf6c0a
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-04 19:23:45 +0300

    Ft: Update login.html template

--- insta/views.py
+++ insta/views.py
@@ -12,7 +12,9 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
     return render(request,'register.html', {'form':form})
 
 def login(request):
+        
     return render(request,'login.html')
