2e089b6e1a72025ac0c5fe05f0d536102f63df88
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-04 18:33:19 +0300

    Create register page function - for new users to sign up

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+def register(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'register.html', {'form':form})
+
+def login(request):
+    return render(request,'login.html')
