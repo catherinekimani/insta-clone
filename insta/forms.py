dafb295d56dcbde16df04a7218c8a44f7229ae2c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-06 11:25:12 +0300

    Create update & newpost forms

--- insta/forms.py
+++ insta/forms.py
@@ -3,8 +3,24 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-
+from . models import *
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+        
+class UpdateProfileForm(forms.ModelForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
+        
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'date_posted']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
