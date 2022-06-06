1ac5a7cf854cde6599165dd4dd57d8231d4522c1
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-06 11:27:51 +0300

    Add urls for addpost & likes

--- insta/urls.py
+++ insta/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index,name='index'),
+    path('',views.index,name='index'),
     
-    path('',views.register,name='register'),
+    path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
     
     path('profile',views.profile,name='profile'),
     
-    path('likes_count/<int:pk>',views.like,name='likes_count')
+    path('likes_count/<int:pk>',views.like,name='likes_count'),
+    
+    path('addPost/', views.addPost, name='addPost'),
 ]
