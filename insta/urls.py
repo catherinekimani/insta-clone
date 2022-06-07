82797d8bd63600fb2527219e412a65924d2225f2
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-07 18:07:06 +0300

    Ft: Solve bugs,Add login functions

--- insta/urls.py
+++ insta/urls.py
@@ -5,11 +5,15 @@ urlpatterns = [
     path('',views.index,name='index'),
     
     path('register/',views.register,name='register'),
-    path('login/',views.login,name='login'),
+    path('login/',views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout'),
     
     path('profile',views.profile,name='profile'),
     
     path('likes_count/<int:pk>',views.like,name='likes_count'),
+    path('comment/',views.new_comment, name = 'comment'),
     
     path('addPost/', views.addPost, name='addPost'),
+    
+    path('search/', views.search_results, name = 'search_results'),
 ]
--- insta/views.py
+++ insta/views.py
@@ -2,13 +2,15 @@ from audioop import reverse
 from django.shortcuts import render,redirect,get_object_or_404
 
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm,NewPostForm
+from .forms import CreateUserForm,NewPostForm, UpdateProfileForm,NewCommentForm,LoginUserForm
 
 from django.contrib.auth.decorators import login_required
 
-from . models import Profile,Post
+from . models import Profile,Post,Comment
 
 from django.http import HttpResponseRedirect
+
+from django.contrib.auth import login,logout,authenticate
 # Create your views here.
 @login_required(login_url='/register/')
 def index(request):
@@ -25,17 +27,42 @@ def register(request):
             return redirect('login')
     return render(request,'register.html', {'form':form})
 
-def login(request):
-    
-    return render(request,'login.html')
+def login_user(request):
+    form = LoginUserForm()
 
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        
+    return render(request, 'login.html', {'form': form})
+
+def logout_user(request):
+    logout (request)
+    return redirect('login')
 @login_required(login_url='/login/')
 def profile(request):
-    profile = Profile.objects.all()
-    return render(request,'profile/profile.html',{'profile':profile})
+    current_user = request.user
+    user_profile = Profile.objects.filter(user=current_user)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('index')
+    else:
+        form =UpdateProfileForm()
+    return render(request,'profile/profile.html',{"form": form,'user_profile':user_profile})
+
 
 @login_required(login_url='/login/')
-def like(request,pk):
+def like(request):
     post = get_object_or_404(Post,id = request.POST.get('post_id'))
     post.likes.add(request.user)
     return redirect('index')
@@ -53,4 +80,30 @@ def addPost(request):
 
     else:
         form = NewPostForm()
-    return render(request, 'addPost.html', {"form": form})
+    return render(request, 'addPost.html', {"user":current_user,"form":form})
+
+def new_comment(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewCommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = current_user
+            comment.save()
+        return redirect('index')
+
+    else:
+        form = NewCommentForm()
+    return render(request, 'comment.html', {"user":current_user,"form": form})
+
+def search_results(request):
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_profiles =Profile.search_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"profile": searched_profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
