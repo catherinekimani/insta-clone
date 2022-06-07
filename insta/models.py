c42639a8f84e9d12d50b58f35cc2e8305e4f7845
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-07 21:04:52 +0300

    Add slight change

--- insta/models.py
+++ insta/models.py
@@ -1,7 +1,5 @@
 from email import contentmanager
-from turtle import title
 from django.db import models
-
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
