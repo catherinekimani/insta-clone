122696753db5f713796a4a75caf84e7a8a39223d
catherinekimani <catherinekimani882@gmail.com>
2024-04-27 21:58:46 +0300

    Refactor search views

--- insta/views.py
+++ insta/views.py
@@ -144,16 +144,16 @@ def new_comment(request, post_id):
     return redirect(request, 'index.html')
 
 def search_results(request):
-    if 'user' in request.GET and request.GET["user"]:
-        search_term = request.GET.get("user")
-        searched_profiles =Profile.search_profile(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html',{"message":message,"profile": searched_profiles})
+    search_term = request.GET.get("q", None)
+    searched_profiles = None
+    if search_term:
+        searched_profiles = Profile.search_profile(search_term)
+        message = f"Found {searched_profiles.count()} users for the search term '{search_term}'"
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
-    
+
+    return render(request, 'search.html', {"message": message, "profile": searched_profiles})
+
 @login_required(login_url='login')
 def follow(request, user_id):
     user = request.user
